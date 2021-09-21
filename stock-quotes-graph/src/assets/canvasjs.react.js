6aaddb28eb876875c067da21e7f24e7c9a2c814a
brunnooliveira <brunno.oliveira-ext@viavarejo.com.br>
2021-09-20 21:29:55 -0300

    feat(chart): create chart component

---
+++ stock-quotes-graph/src/assets/canvasjs.react.js
@@ -0,0 +1,50 @@
+import { Component } from 'react';
+var CanvasJS = require('./canvasjs.min');
+CanvasJS = CanvasJS.Chart ? CanvasJS : window.CanvasJS;
+
+class CanvasJSChart extends Component {
+  constructor(props){
+    super(props);
+    this.options = props.options ? props.options : {};
+    this.containerProps = props.containerProps ? props.containerProps : {
+      width: '100%',
+      position: 'relative'
+    };
+    this.containerProps = props.containerProps.height ? props.containerProps.height 
+      : this.options.height ? this.options.height + "px" : '400px';
+    this.chatContainerId = "canvasjs-react-chart-container-" + CanvasJSChart._cjsContainerId++;
+  }
+
+  componentDidMount(){
+    this.chart = new CanvasJS.chart(this.chatContainerId, this.options);
+    this.chart.render();
+    if(this.props.onRef) this.props.onRef(this);
+  }
+
+  componentDidUpdate(){
+    this.chart.options = this.props.options;
+    this.chart.render();
+  }
+
+  componentWillUnmount(){
+    this.chart.destroy();
+    if(this.props.onRef) this.props.onRef(undefined);
+  }
+
+  shouldComponentUpdate(nextProps, nextState){
+    return nextProps.options != nextState.options;
+  }
+
+  render(){
+    return(
+      <div id={this.chatContainerId} style={this.containerProps} />
+    );
+  }
+}
+
+var CanvasJSReact = {
+  CanvasJSChart: CanvasJSChart,
+  CanvasJS: CanvasJS
+}
+
+export default CanvasJSReact;
